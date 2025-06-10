f441c7d9c798c819d018ad85d689a731d3916a7a
Dang Vu <tamquattnb@gmail.com>
2025-06-10 21:48:22 +0100

    update R3D model to use KINETICS400_V1 weights for improved performance

--- models/r3d.py
+++ models/r3d.py
@@ -3,6 +3,6 @@ import torchvision.models.video as video_models
 
 
 def build_r3d_model(num_classes):
-    model = video_models.r3d_18(pretrained=True)
+    model = video_models.r3d_18(weights="KINETICS400_V1")
     model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
