2a6a6cd66fba0d9ed181e2efa6d942e3fe5c4a05
Dang Vu <tamquattnb@gmail.com>
2025-06-13 00:08:48 +0100

    refactor training script to load configuration from YAML and use dynamic parameters

--- train.py
+++ train.py
@@ -2,10 +2,15 @@ import pandas as pd
 import torch
 from torch.utils.data import DataLoader
 from torch import optim, nn
+import yaml
 
 from data.dataset import CharadesTAL
 from data.transforms import get_default_transform
 from models.r3d import build_r3d_model
+
+# Load config.yaml
+with open("charades_tal/configs/config.yaml", "r") as f:
+    config = yaml.safe_load(f)
 
 # Load annotation & preprocess
 anno_df = pd.read_csv("CharadesEgo/CharadesEgo_v1_train.csv")  # Change dir if necessary
@@ -34,19 +39,22 @@ cls2idx = {cls: idx for idx, cls in enumerate(filtered_actions)}
 # Instantiate dataset
 dataset = CharadesTAL(
     filtered,
-    "CharadesEgo_v1_rgb/CharadesEgo_v1_rgb",
+    config["frame_root"],
     transform=get_default_transform(),
     cls2idx=cls2idx,
+    clip_len=config["clip_len"],
+    stride=config["stride"],
+    fps=config["fps"],
 )
-loader = DataLoader(dataset, batch_size=4, shuffle=True)
+loader = DataLoader(dataset, batch_size=config["batch_size"], shuffle=True)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = build_r3d_model(len(filtered_actions)).to(device)
-opt = optim.Adam(model.parameters(), lr=1e-4)
+opt = optim.Adam(model.parameters(), lr=config["lr"])
 crit = nn.CrossEntropyLoss()
 
 # Training loop
-for epoch in range(1):
+for epoch in range(config["epochs"]):
     model.train()
     total_loss = 0
     for clips, labels in loader:
